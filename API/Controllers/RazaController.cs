ed6fb705b3b1b02cc8b856c46e19c019643913a3
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-10-11 07:29:10 -0500

    feat: :ambulance: Introduce new code RazaController

--- API/Controllers/RazaController.cs
+++ API/Controllers/RazaController.cs
@@ -46,6 +46,53 @@ namespace API.Controllers;
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<Raza>> Post(RazaDto razaDto)
         {
+            var raza = _mapper.Map<Raza>(razaDto);
+            this._unitOfWork.Raza.Add(raza);
+            await _unitOfWork.SaveAsync();
+            if(raza == null)
+            {
+                return BadRequest();
+            }
+            razaDto.Id = raza.Id;
             return CreatedAtAction(nameof(Post), new {id = razaDto.Id}, razaDto);
         }
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<RazaDto>> Put(int id, [FromBody] RazaDto razaDto)
+        {
+            var raza = _mapper.Map<Raza>(razaDto);
+            if(raza == null)
+            {
+                return NotFound();
+            }
+            if(raza.Id == 0)
+            {
+                raza.Id = id;
+            }
+            if(raza.Id != id)
+            {
+                return BadRequest();
+            }
+            razaDto.Id = raza.Id;
+            _unitOfWork.Raza.Update(raza);
+            await _unitOfWork.SaveAsync();
+            return razaDto;
+        }
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id )
+        {
+            var raza = await _unitOfWork.Raza.GetByIdAsync(id);
+            if(raza == null)
+            {
+                return NotFound();            
+            
+            }
+            _unitOfWork.Raza.Remove(raza);
+            await _unitOfWork.SaveAsync();        
+            return NoContent();
+            }
     }
